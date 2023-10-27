4d3c7b299be5cbca70dc842a8e33896bcac3b890
Shubhangiverma25 <shubhangiverma46148@gmail.com>
2023-10-27 13:38:47 +0530

    counter value

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -4,14 +4,21 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  let counter = 5;
+
+  const addValue = () => {
+    console.log("clicked", Math.random());
+    counter = counter + 1;
+  };
+
   return (
     <>
-      <h1>Chai Aur React Counter</h1>
-      <h2>Counter Value : 5</h2>
-
-      <button>Add Value</button>
+      <h1>Chai Aur React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value{counter}</button>
       <br />
-      <button>Remove Value</button>
+      <button>Remove Value{counter}</button>
+      <p>footer: {counter}</p>
     </>
   );
 }
