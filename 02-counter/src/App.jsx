db6e25b7829eff5dfc46aae61d3f5d52a3430c6c
Shubhangiverma25 <shubhangiverma46148@gmail.com>
2023-10-27 13:40:30 +0530

    changes counter

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -4,11 +4,17 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  let counter = 5;
+  let [counter, setCounter] = useState(5);
+
+  //let counter = 5;
 
   const addValue = () => {
-    console.log("clicked", Math.random());
-    counter = counter + 1;
+    //counter = counter + 1;
+    setCounter(counter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
   };
 
   return (
@@ -17,7 +23,7 @@ function App() {
       <h2>Counter value: {counter}</h2>
       <button onClick={addValue}>Add Value{counter}</button>
       <br />
-      <button>Remove Value{counter}</button>
+      <button onClick={removeValue}>Remove Value{counter}</button>
       <p>footer: {counter}</p>
     </>
   );
