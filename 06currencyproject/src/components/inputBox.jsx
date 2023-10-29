4f97de502a5e0fe220fdc5f032bea68d9bec62ad
Shubhangiverma25 <shubhangiverma46148@gmail.com>
2023-10-29 17:08:48 +0530

    currency project

---
+++ 06currencyproject/src/components/inputBox.jsx
@@ -0,0 +1,52 @@
+import React, { useId } from "react";
+
+function InputBox({
+  label,
+  amount,
+  onAmtChange,
+  onCurrChange,
+  currOption = [],
+  selectCurr = "usd",
+  amtDisabled = false,
+  currDisabled = false,
+
+  className = "",
+}) {
+  const amtInputId = useId();
+
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label htmlFor={amtInputId} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
+        <input
+          id={amtInputId}
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amtDisabled}
+          value={amount}
+          onChange={(e) => onAmtChange && onAmtChange(Number(e.target.value))}
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurr}
+          onChange={(e) => onCurrChange && onCurrChange(e.target.value)}
+          disabled={currDisabled}
+        >
+          {currOption.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
